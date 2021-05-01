a05416f03ac32bbdbd1936cd5f6e6d6e701bea29
faeshal_ <faeshal@deeeplabs.id>
2021-05-01 14:09:29 +0700

    add a bunc

--- src/server.js
+++ src/server.js
@@ -3,10 +3,31 @@ const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
 const log4js = require("log4js");
+const morgan = require("morgan");
+const passport = require("passport");
 const log = log4js.getLogger("entrypoint");
 log.level = "info";
+const session = require("express-session");
 const authRoute = require("./route/auth");
 const userRoute = require("./route/user");
+
+// * Basick pkg
+app.use(express.json());
+app.use(morgan());
+
+// * Session
+app.use(
+  session({
+    secret: "tdVEZXp7DD",
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // * Routing
 app.use(authRoute);
@@ -20,7 +41,6 @@ app.get("*", (req, res) => {
   res.status(404).json({ success: false, message: "Nothing In this Route..." });
 });
 
-// * Database
 // * Database Connection
 mongoose.connect("mongodb://localhost:27017/passport", {
   useNewUrlParser: true,
